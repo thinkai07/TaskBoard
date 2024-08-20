1dd8fa624f652014393a06d60186e8f7b6964bb3
Siva2591 <sunkarivenkatasiva638@gmail.com>
2024-08-20 19:08:29 +0530

    added

--- server/models/Activity.js
+++ server/models/Activity.js
@@ -15,13 +15,11 @@ const activitySchema = new mongoose.Schema(
   }
 );
 
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 module.exports = mongoose.model("Activity", activitySchema);
-=======
+
 module.exports = mongoose.model("Activity", activitySchema);
->>>>>>> e2080dec6c5ef2d42c0feb4e3c203f5c14403dfe
-=======
+
 module.exports = mongoose.model("Activity", activitySchema);
 
 
@@ -29,4 +27,4 @@ module.exports = mongoose.model("Activity", activitySchema);
 
 
 
->>>>>>> 8153153255c5360b0a271a54212e5094728a3356
+
